ed65fba5ffbd97609758be1169b9bc7456d83dde
mediba-kitada <kitada@mediba.jp>
2016-02-02 23:48:58 +0900

    update version

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,11 @@
 default['gatling']['user']            = 'gatling'
 default['gatling']['group']           = 'gatling'
 default['gatling']['install_prefix']  = '/usr/local'
-default['gatling']['download_url']    = 'INSERT GATLING DOWNLOAD URL HERE'       # fill this in from http://gatling-tool.org/
-default['gatling']['version']         = '2.0.0-M3a'                              # fill this in too
+default[:gatling][:download_url] = 'https://repo1.maven.org/maven2/io/gatling/highcharts/gatling-charts-highcharts-bundle/2.1.7/gatling-charts-highcharts-bundle-2.1.7-bundle.zip'
+default[:gatling][:version] = '2.1.7'
 
 # Gatling 2 requires Java 7
 default["java"]["install_flavor"] = "oracle"
-default["java"]["jdk_version"] = 7
+default["java"]["jdk_version"] = 8
 default["java"]["oracle"]["accept_oracle_download_terms"] = true
+
